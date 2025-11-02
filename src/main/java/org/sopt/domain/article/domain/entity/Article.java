74338231c2069450310775a447eeb913e8ff5319
Kimgyuilli <rlarbdlf222@gmail.com>
2025-11-02 19:38:19 +0900

    feat: Article 객체 생성

--- src/main/java/org/sopt/domain/article/domain/entity/Article.java
+++ src/main/java/org/sopt/domain/article/domain/entity/Article.java
@@ -1,20 +1,29 @@
 package org.sopt.domain.article.domain.entity;
 
+import org.sopt.domain.member.domain.entity.Member;
+import org.sopt.global.entity.BaseTimeEntity;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Article {
+public class Article extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Member author;
 
-
+	private Tag tag;
+	private String title;
+	private String content;
 }
