b4e5ea9c1d84b79c238e9ba8a61ab22d6a6654e3
Kimgyuilli <rlarbdlf222@gmail.com>
2025-11-02 18:59:05 +0900

    refactor: MemberService Interface 제거

--- src/main/java/org/sopt/member/application/service/MemberService.java
+++ src/main/java/org/sopt/member/application/service/MemberService.java
@@ -1,13 +1,54 @@
 package org.sopt.member.application.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.sopt.global.exception.BaseException;
+import org.sopt.global.response.error.ErrorCode;
 import org.sopt.member.application.dto.MemberCreateRequest;
 import org.sopt.member.application.dto.MemberResponse;
+import org.sopt.member.domain.entity.Member;
+import org.sopt.member.domain.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface MemberService {
-	MemberResponse join(MemberCreateRequest request);
-	MemberResponse findMember(Long memberId);
-	List<MemberResponse> findAllMembers();
-	void deleteMember(String email);
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public MemberResponse join(MemberCreateRequest request) {
+        if (memberRepository.existsByEmail(request.email())) {
+            throw new BaseException(ErrorCode.DUPLICATE_EMAIL);
+        }
+
+        Member member = Member.create(request);
+        memberRepository.save(member);
+        return MemberResponse.from(member);
+    }
+
+    public MemberResponse findMember(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new BaseException(ErrorCode.MEMBER_NOT_FOUND));
+        return MemberResponse.from(member);
+    }
+
+    public List<MemberResponse> findAllMembers() {
+        return memberRepository.findAll().stream()
+                .map(MemberResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void deleteMember(String email) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new BaseException(ErrorCode.MEMBER_NOT_FOUND));
+        memberRepository.deleteByEmail(member.getEmail());
+    }
 }
