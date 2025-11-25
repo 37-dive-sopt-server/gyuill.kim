b9ae32d08843c52905b0de5b231390444708f628
Kimgyuilli <rlarbdlf222@gmail.com>
2025-11-25 22:54:38 +0900

    feat(oauth2): securityConfig에 Oauth2 설정 추가

--- src/main/java/org/sopt/global/config/SecurityConfig.java
+++ src/main/java/org/sopt/global/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package org.sopt.global.config;
 
 import org.sopt.global.auth.exception.JwtAuthenticationEntryPoint;
 import org.sopt.global.auth.jwt.JwtAuthenticationFilter;
+import org.sopt.global.auth.oauth2.handler.OAuth2AuthenticationFailureHandler;
+import org.sopt.global.auth.oauth2.handler.OAuth2AuthenticationSuccessHandler;
+import org.sopt.global.auth.oauth2.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -21,6 +24,9 @@ public class SecurityConfig {
 
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+	private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,10 +37,18 @@ public class SecurityConfig {
 			)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/auth/**").permitAll()
+				.requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
 				.requestMatchers(HttpMethod.POST, "/members").permitAll()
 				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**").permitAll()
 				.requestMatchers("/members/**", "/articles/**").authenticated()
 				.anyRequest().denyAll()
+			)
+			.oauth2Login(oauth2 -> oauth2
+				.userInfoEndpoint(userInfo -> userInfo
+					.userService(customOAuth2UserService)
+				)
+				.successHandler(oAuth2AuthenticationSuccessHandler)
+				.failureHandler(oAuth2AuthenticationFailureHandler)
 			)
 			.exceptionHandling(exception -> exception
 				.authenticationEntryPoint(jwtAuthenticationEntryPoint)
