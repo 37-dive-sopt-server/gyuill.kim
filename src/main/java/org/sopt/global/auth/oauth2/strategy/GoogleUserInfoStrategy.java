0ce76f85d027fce36c63454d9984178a7357107d
Kimgyuilli <rlarbdlf222@gmail.com>
2025-11-25 22:43:14 +0900

    feat(oauth): 소셜로그인 strategy 클래스 구현

---
+++ src/main/java/org/sopt/global/auth/oauth2/strategy/GoogleUserInfoStrategy.java
@@ -0,0 +1,15 @@
+package org.sopt.global.auth.oauth2.strategy;
+
+import java.util.Map;
+import org.sopt.global.auth.oauth2.dto.GoogleUserInfo;
+import org.sopt.global.auth.oauth2.dto.OAuth2UserInfo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GoogleUserInfoStrategy implements OAuth2UserInfoStrategy {
+
+	@Override
+	public OAuth2UserInfo extractUserInfo(Map<String, Object> attributes) {
+		return new GoogleUserInfo(attributes);
+	}
+}
