077b5cad722ed2c8444cf8cbaaf70cad2e3b4301
Kimgyuilli <rlarbdlf222@gmail.com>
2025-11-25 22:53:15 +0900

    feat(oauth2): CustomUserDetail에 oauth2 필드 추가

--- src/main/java/org/sopt/global/auth/security/CustomUserDetails.java
+++ src/main/java/org/sopt/global/auth/security/CustomUserDetails.java
@@ -2,22 +2,29 @@ package org.sopt.global.auth.security;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class CustomUserDetails implements UserDetails {
+public class CustomUserDetails implements UserDetails, OAuth2User {
 
 	private final Long id;
 	private final String email;
 	private final String password;
+	private Map<String, Object> attributes;
 
 	public Long getId() {
 		return id;
+	}
+
+	public void setAttributes(Map<String, Object> attributes) {
+		this.attributes = attributes;
 	}
 
 	@Override
@@ -53,5 +60,15 @@ public class CustomUserDetails implements UserDetails {
 	@Override
 	public boolean isEnabled() {
 		return true;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
+
+	@Override
+	public String getName() {
+		return email;
 	}
 }
