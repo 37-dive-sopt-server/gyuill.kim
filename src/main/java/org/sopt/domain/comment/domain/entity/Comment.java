4c555875edbda3f76a6da6775b0e77c59cec8c69
Kimgyuilli <rlarbdlf222@gmail.com>
2025-12-12 20:01:35 +0900

    refactor (comment): 코드 포맷ã팅

--- src/main/java/org/sopt/domain/comment/domain/entity/Comment.java
+++ src/main/java/org/sopt/domain/comment/domain/entity/Comment.java
@@ -1,5 +1,11 @@
 package org.sopt.domain.comment.domain.entity;
 
+import org.sopt.domain.article.domain.entity.Article;
+import org.sopt.domain.comment.exception.CommentException;
+import org.sopt.domain.member.domain.entity.Member;
+import org.sopt.global.entity.BaseTimeEntity;
+import org.sopt.global.response.error.ErrorCode;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,17 +19,12 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.sopt.domain.article.domain.entity.Article;
-import org.sopt.domain.member.domain.entity.Member;
-import org.sopt.global.entity.BaseTimeEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "comment", indexes = {
-	@Index(name = "idx_comment_article", columnList = "article_id"),
-	@Index(name = "idx_comment_author", columnList = "author_id")
-})
+@Table(name = "comment", indexes = {@Index(name = "idx_comment_article", columnList = "article_id"),
+	@Index(name = "idx_comment_author", columnList = "author_id")})
 public class Comment extends BaseTimeEntity {
 
 	@Id
@@ -61,9 +62,7 @@ public class Comment extends BaseTimeEntity {
 
 	public void validateAuthor(Long requesterId) {
 		if (!isAuthor(requesterId)) {
-			throw new org.sopt.domain.comment.exception.CommentException(
-				org.sopt.global.response.error.ErrorCode.COMMENT_UNAUTHORIZED
-			);
+			throw new CommentException(ErrorCode.COMMENT_UNAUTHORIZED);
 		}
 	}
 }
